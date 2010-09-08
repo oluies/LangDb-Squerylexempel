0aeee454306d28ab33520d10633b64169a7e05aa
Nikolaj Lindberg <nikolaj.lindberg@gmail.com>
2010-09-08 21:27:20 +0200

    kdlkfdlk

--- project/build/SquerylDemoProject.scala
+++ project/build/SquerylDemoProject.scala
@@ -2,7 +2,6 @@ import sbt._
 
 class SquerylDemoProject(info: ProjectInfo) extends DefaultProject(info)
 {
-  //lazy val hi = task { println("Hello World"); None }
   val h2 = "com.h2database" % "h2" % "1.2.141"
   val cglib = "cglib" % "cglib-nodep" % "2.2"
   //val squeryl = "org.squeryl" % "squeryl_2.8.0" % "0.9.4beta8"
