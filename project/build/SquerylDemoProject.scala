6fd1fc8afebdd50ce75ef6cd67854b70cd6664fb
Nikolaj Lindberg <nikolaj.lindberg@gmail.com>
2010-09-12 23:00:21 +0200

    Ändrat till Squeryl 0.9.4-RC1 i projektfilen

--- project/build/SquerylDemoProject.scala
+++ project/build/SquerylDemoProject.scala
@@ -4,6 +4,6 @@ class SquerylDemoProject(info: ProjectInfo) extends DefaultProject(info)
 {
   val h2 = "com.h2database" % "h2" % "1.2.141"
   val cglib = "cglib" % "cglib-nodep" % "2.2"
-  //val squeryl = "org.squeryl" % "squeryl_2.8.0" % "0.9.4beta8"
+  val squeryl = "org.squeryl" % "squeryl_2.8.0" % "0.9.4-RC1"
   val specs = "org.scala-tools.testing" % "specs_2.8.0" % "1.6.5"
 }
