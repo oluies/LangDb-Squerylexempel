35130d95a841d86e74c69b25d362af78a09a0b8c
Hanna Lindgren <hanna@dressinen.(none)>
2010-10-01 18:55:58 +0200

    kodkommentarer/scaladoc

--- src/main/scala/se/stts/langdb/LangDb.scala
+++ src/main/scala/se/stts/langdb/LangDb.scala
@@ -1,113 +1,197 @@
-package se.stts.langdb // TA BORT FRAN KODRUTA
+package se.stts.langdb
 
-import org.squeryl._ // TA BORT FRAN KODRUTA
-import org.squeryl.dsl._ // TA BORT FRAN KODRUTA
-import dsl.CompositeKey2 // TA BORT FRAN KODRUTA
-import annotations._ // TA BORT FRAN KODRUTA
+import org.squeryl._
+import org.squeryl.dsl._
+import dsl.CompositeKey2
+import annotations._
 
-// This import gives you 'oneToManyRelation', '===', etc...
-import org.squeryl.PrimitiveTypeMode._  // TA BORT FRAN KODRUTA
+// Denna importrad ger tillgång till Squeryls 'oneToManyRelation', '===', etc...
+import org.squeryl.PrimitiveTypeMode._ 
 
 
-// A Scala 'trait' is like a mix of Java's 'abtract class' and
-// 'interface.  Exending following trait, gives a Squeryl DB object an
-// auto-incremented primary key, 'id', of type Int:
- 
+/**
+ * Scalas 'trait' är ett koncept för att återanvända kod på ett
+ * flexibelt sätt. Genom att implementera följande trait, <code>AutoId</code>,
+ * skapas en primärnyckel, <code>id</code> av typen Int, som räknas upp automatiskt.
+ */
 trait AutoId extends KeyedEntity[Int] {
+  /**
+   * @param id Tabellens primärnyckel
+   */
   val id: Int = 0
 }
 
 
-
-// Declaring a 'case class' adds functionality to a class. For
-// example, 'getters and setters' for constructor arguments and other
-// nice stuff.
+/**
+ * En 'case-klass' som representerar ett språk.
+ *
+ * 'case' ger en klass lite extra funktionalitet. Några exempel:
+ * <ul>
+ * <li><code>equals</code> och <code>hashCode</code></li>
+ * <li>getters/setters</li>
+ * <li><code>toString</code></li>
+ * </ul>
+ * 
+ * @param iso Språkets ISO-kod (tre tecken, ISO 639-2)
+ * @param enName Språkets engelska namn
+ * @param cBlock Språkets 'code block' (teckenuppsättning),
+ *               till exempel LATIN eller CYRILLIC.
+ * 
+ */
 
 case class Lang(iso: String, enName: String, cBlock: Option[String]) extends AutoId {
-  // 'Option' construnctor parameters make the following zero-argument
-  // default constructor necessary (this is a Squeryl thing):
-  def this() = this("", "", Some("")) // TA BORT FRAN KODRUTA
-  // TA BORT FRAN KODRUTA
+
+  /** Om konstruktorn tar 'Option'-parametrar, behövs en extra
+   konstruktor utan argument (Squeryl behöver detta, tills vidare): */
+   def this() = this("", "", Some(""))
+ 
+  /**
+   * Koppling till språkets 'alternativa' namn via <em>en till många</em>-relationen <code>langToAltNames</code>
+   */
   lazy val aNames: OneToMany[AltName] = LangDb.langToAltNames.left(this)
+
+  /**
+   * Koppling till språkfamiljer namn via <em>många till många</em>-relationen <code>langsAndFamilies</code>
+   */
   lazy val families = LangDb.langsAndFamilies.left(this)
+
+  /**
+   * Koppling till länder via <em>många till många</em>-relationen <code>langsAndCountries</code>
+   */
   lazy val countries = LangDb.langsAndCountries.left(this)
 }
 
+/**
+ * Alternativa (icke-engelska) namn på språk.
+ * @param name Namnet
+ * @param langId Språkets databas-id (se <code>Lang</code>)
+ */
 case class AltName(name: String, langId: Int = 0) extends AutoId {
+  /**
+   * Koppling till språket via <em>en till många</em>-relationen <code>langToAltNames</code>
+   */ 
   lazy val enLang: Lang = LangDb.langToAltNames.right(this).single
 }
 
-
+/**
+ * Ett land.
+ * @param iso Landets ISO-kod (två tecken, ISO 3166-1 alpha 2)
+ * @param enName Landets namn
+ * @param area Region/världsdel
+ */
 case class Country(iso: String, enName: String, area: Option[String]) extends AutoId 
-{ // TA BORT FRAN KODRUTA
-  def this() = this("", "", Some(""))  // TA BORT FRAN KODRUTA
-} // TA BORT FRAN KODRUTA
+{
+  def this() = this("", "", Some("")) 
+}
 
 
-// ISO codes for language families: ISO 639-5 
+/**
+ * En språkfamilj (indoeuropeiska, germanska, arabiska, etc)
+ * @param name Språkfamiljens namn
+ * @param iso Språkfamiljens ISO-kod (tre tecken; ISO 639-5)
+ */
 case class Family(name: String, iso: Option[String]) extends AutoId 
-{ // TA BORT FRAN KODRUTA
-  def this() = this("", Some("")) // TA BORT FRAN KODRUTA
-} // TA BORT FRAN KODRUTA
+{
+  def this() = this("", Some(""))
+}
 
-
-// Association table between Lang and Country, between which there is a many-to-many relationship:
+/**
+ * Kopplingstabell mellan <code>Lang</code> och <code>Språk</code>, mellan vilka det råder en <em>många till många</em>-relation.
+ * @param official Anger om språket har officiell status i aktuellt land
+ * @param speakers Antalet talare av språket, i aktuellt land
+ * @param langId Språkets databas-id
+ * @param countryId Landets databas-id
+ */
 case class LangAndCountry(official: Option[Boolean], speakers: Option[Long], 
                           langId: Int = 0, countryId: Int = 0) 
   extends KeyedEntity[CompositeKey2[Int,Int]] {
-  def this() = this(Some(false), Some(0), 0, 0) // TA BORT FRAN KODRUTA
+  def this() = this(Some(false), Some(0), 0, 0)
   def id = compositeKey(countryId, langId)
 }
 
-// Association table between Lang and Family, between which there is a many-to-many relationship:
+/**
+ * Kopplingstabell mellan <code>Lang</code> och <code>Family</code>, mellan vilka det råder en <em>många till många</em>-relation.
+ * @param langId Språkets databas-id
+ * @param familyId Språkfamiljens databas-id
+ */
 case class LangAndFamily(langId: Int = 0, familyId: Int = 0) 
   extends KeyedEntity[CompositeKey2[Int,Int]] {
   def id = compositeKey(langId, familyId)
 }
 
+/**
+ * Schema för databasen. Här deklareras databasens tabeller och relationerna dem emellan.
+ */
 object LangDb extends Schema {
-  // TA BORT FRAN KODRUTA
-  //
-  // Basic data tables
-  //
+ 
+  /**
+   * Grundläggande tabeller
+   */
+
+  /**
+   * Tabell för <code>Lang</code>
+   */
   val langs: Table[Lang] = table[Lang]
+
+  /**
+   * Tabell för <code>AltName</code>
+   */
   val altNames = table[AltName]
+
+  /**
+   * Tabell för <code>Country</code>
+   */
   val countries = table[Country]
+
+  /**
+   * Tabell för <code>Family</code>
+   */
   val families = table[Family]
 
-  on(langs)(l => declare(columns(l.iso) are(unique,indexed)))// TA BORT FRAN KODRUTA
-  // funkar inte: on(langs)(l => declare(l.iso is(unique,indexed)))// TA BORT FRAN KODRUTA
-  on(countries)(c => declare(columns(c.iso) are(unique,indexed)))// TA BORT FRAN KODRUTA
-  on(families)(f => declare(// TA BORT FRAN KODRUTA
-    columns(f.iso) are(unique,indexed),// TA BORT FRAN KODRUTA
-    columns(f.name) are(unique,indexed)// TA BORT FRAN KODRUTA
-  ))// TA BORT FRAN KODRUTA
-  on(altNames)(a => declare(// TA BORT FRAN KODRUTA
-    columns(a.langId, a.name) are(unique, indexed)// TA BORT FRAN KODRUTA
-  ))// TA BORT FRAN KODRUTA
-  // TA BORT FRAN KODRUTA
-  //
-  // Relations
-  //
-  // TA BORT FRAN KODRUTA
-  //
-  // Association tables (for many-to-many mappings between two tables)
-  //
-  // langAndCountry is a ManyToManyRelation, it extends Table[LangAndCountry]
-  // TA BORT FRAN KODRUTA  
-  val langsAndCountries = 
+  /**
+   * 'Constraints' för några tabeller
+   */
+  //on(langs)(l => declare(columns(l.iso) are(unique,indexed)))
+  on(langs)(l => declare(l.iso is(unique,indexed))) // funkar inte?
+  on(countries)(c => declare(columns(c.iso) are(unique,indexed)))
+  on(families)(f => declare(
+    columns(f.iso) are(unique,indexed),
+    columns(f.name) are(unique,indexed)
+  ))
+  on(altNames)(a => declare(
+    columns(a.langId, a.name) are(unique, indexed)
+  ))
+ 
+  /**
+   * Relationer/kopplingstabeller
+   */
+ 
+  /** 
+   * <code>langsAndCountries</code> är en <code>ManyToManyRelation</code>,
+   * som är av typen <code>Table[LangAndCountry]</code>
+   */
+   val langsAndCountries = 
     manyToManyRelation(langs, countries).via[LangAndCountry]((l, c, lic) => 
       (l.id === lic.langId, c.id === lic.countryId))
 
+  /** 
+   * <code>langsAndFamilies</code> är en <code>ManyToManyRelation</code>,
+   * som är av typen <code>Table[LangAndFamily]</code>
+   */
   val langsAndFamilies = 
     manyToManyRelation(langs, families).via[LangAndFamily]((l, f, laf) => 
       (l.id === laf.langId, f.id === laf.familyId))
 
+  /** 
+   * <code>langToAltNames</code> är en <code>OneToManyRelation</code>
+   */
   val langToAltNames = 
     oneToManyRelation(langs, altNames).via((l, a) => l.id === a.langId)
-  // TA BORT FRAN KODRUTA
-  // Makes it possible to drop (delete) the whole db. Use wisely.
-  override def drop = super.drop // TA BORT FRAN KODRUTA
+ 
+  /**
+   * Raderar hela databasen. Tänk efter före!
+   */
+  override def drop = super.drop
 }
 
 
